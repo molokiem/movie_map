a97cc5797542aca6ae3e94d626c6fc148590a0b5
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-03-21 13:59:01 -0400

    HTML wireframe

--- js/script.js
+++ js/script.js
@@ -2,13 +2,28 @@ var moveLeftButton = document.querySelector(".move-left-button");
 
 var moveMovieTickerLeft = document.querySelector(".john-wick-4-link");
 
-
 moveLeftButton.addEventListener("click", moveLeft);
 
 function moveLeft() {
+  moveMovieTickerLeft.setAttribute("style", "right: 395px;");
 
-    moveMovieTickerLeft.setAttribute("style", "right: 395px;");
-    
-    console.log(moveLeft)
+  console.log(moveLeft);
 }
 
+//side note
+//getLocation button
+
+// navigator.geolocation.getCurrentPosition(function (locationData) {
+//   console.log(locationData);
+//   //made a request for the api to get current location of the user
+//   $.get(
+//     url +
+//       "&lat=" +
+//       locationData.coords.latitude +
+//       "&lon=" +
+//       locationData.coords.longitude
+//   );
+//   then(function (data) {
+//     console.log(data);
+//   });
+// });
